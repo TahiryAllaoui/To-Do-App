d8ca45b731182ef332b7aee9ecb1bac6321c69bd
TahiryAllaoui <tallaoui31@gmail.com>
2023-10-15 10:52:23 +0300

    update

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -26,35 +26,26 @@ const ToDoList = ({ toDos }: { toDos: ITodo[] }) => {
     })
       .then((res) => res.json())
       .then((data: any) => {
-        let tmp: ITodo[] = [];
-        data.forEach((index: any) => {
-          let newItem: ITodo = {
-            id: index._id,
-            content: index.content,
-            createdAt: index.createdAt,
-          };
-          tmp.push(newItem);
-          console.log(tmp);
-        });
+        console.log(data);
+        // let tmp: ITodo[] = [];
+        // data.forEach((index: any) => {
+        //   let newItem: ITodo = {
+        //     id: index._id,
+        //     content: index.content,
+        //     createdAt: index.createdAt,
+        //   };
+        //   tmp.push(newItem);
+        //   console.log(tmp);
       });
-    let pop = document.querySelector(".pop") as HTMLElement;
-    pop.style.display = "flex";
-    pop.style.flexDirection = "column";
-    pop.style.justifyContent = "center";
-    pop.style.alignItems = "center";
   };
 
-  const handleBack = () => {
-    let pop = document.querySelector(".pop") as HTMLElement;
-    pop.style.display = "none";
-    console.log("back");
-  };
-
-  const handleDelete = () => {
-    fetch("http://localhost:3000", {
+  const handleDelete = (id: string) => {
+    fetch("http://localhost:3000/" + id, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
-    });
+    })
+      .then(() => console.log("deleted"))
+      .catch((e) => console.log("err: " + e));
   };
 
   return (
@@ -68,20 +59,11 @@ const ToDoList = ({ toDos }: { toDos: ITodo[] }) => {
           <div className="todoItem">
             <p onClick={() => handleSingleItem(todo.id)}>{todo.content}</p>
           </div>
-          <div className="pop">
-            <h2>{todo.content}</h2>
-            <p>On: {todo.createdAt}</p>
-            <p
-              style={{ color: "gray", border: "1px solid black" }}
-              onClick={handleBack}
-            >
-              Back
-            </p>
-          </div>
+
           <div
             className="delete"
             style={{ color: "gray", cursor: "pointer" }}
-            onClick={handleDelete}
+            onClick={() => handleDelete(todo.id)}
           >
             Delete
           </div>
